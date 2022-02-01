bd10a8a5eee11bb5e5f69694b34638984b0f2a05
SuraZz007 <surajdev9886@gmail.com>
2022-02-01 23:26:08 +0530

    Navar for all category made using useState fetched from API

--- src/component/Basics/Restaurant.js
+++ src/component/Basics/Restaurant.js
@@ -18,10 +18,14 @@ import MenuCard from './MenuCard';
       <div className="btn-group">
       <button className="btn-group__item" 
               onClick={()=>filterItem("breakfast")}>Breakfast</button>
-      <button className="btn-group__item">Lunch</button>
-      <button className="btn-group__item">Evening</button>
-      <button className="btn-group__item">Dinner</button>
-      <button className="btn-group__item">All</button>
+      <button className="btn-group__item"
+              onClick={()=>filterItem("lunch")}>Lunch</button>
+      <button className="btn-group__item"
+               onClick={()=>filterItem("evening")}>Evening</button>
+      <button className="btn-group__item"
+               onClick={()=>filterItem("dinner")}>Dinner</button>
+      <button className="btn-group__item"
+               onClick={()=> setMenuData(Menu)}>All</button>
           
       </div>
   </nav>
