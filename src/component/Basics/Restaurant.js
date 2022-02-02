e2c890141d1153799364c29b9eb56c043208f10b
SuraZz007 <surajdev9886@gmail.com>
2022-02-02 15:43:20 +0530

    Particular category called from api using props

--- src/component/Basics/Restaurant.js
+++ src/component/Basics/Restaurant.js
@@ -2,9 +2,20 @@
  import './style.css'
  import Menu from './menuApi';
 import MenuCard from './MenuCard';
+import Navbar from './Navbar';
+
+const uniqueList = [
+  ...new Set(
+    Menu.map((curElem)=>{
+      return curElem.category;
+    })
+  )
+]
+console.log(uniqueList);
  
  const Restaurant = () => {
      const [menuData, setMenuData] = useState(Menu);
+     const [menuList, setMenuList] = useState(uniqueList);
   
      const filterItem = (category) => {
        const updatedList = Menu.filter((curElem)=>{
@@ -14,21 +25,7 @@ import MenuCard from './MenuCard';
      }
    return (
   <>
-  <nav className="navbar">
-      <div className="btn-group">
-      <button className="btn-group__item" 
-              onClick={()=>filterItem("breakfast")}>Breakfast</button>
-      <button className="btn-group__item"
-              onClick={()=>filterItem("lunch")}>Lunch</button>
-      <button className="btn-group__item"
-               onClick={()=>filterItem("evening")}>Evening</button>
-      <button className="btn-group__item"
-               onClick={()=>filterItem("dinner")}>Dinner</button>
-      <button className="btn-group__item"
-               onClick={()=> setMenuData(Menu)}>All</button>
-          
-      </div>
-  </nav>
+    <Navbar filterItem={filterItem} menuList={menuList}/>
     <MenuCard menuData={menuData} />
   </>
    )
