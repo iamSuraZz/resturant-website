a289dc1ee29a41b8adedab866a693593c5bbe0b1
SuraZz007 <surajdev9886@gmail.com>
2022-02-04 16:13:53 +0530

    Add various icons according to weather

--- src/component/weather/temp.js
+++ src/component/weather/temp.js
@@ -22,6 +22,58 @@ const Temp = () => {
         <div className="weatherIcon">
           <i className={"wi wi-day-sunny"}></i>
         </div>
+        <div className="weatherInfo">
+          <div className="temperature">
+            <span>25.5&deg;</span>
+          </div>
+
+          <div className="description">
+            <div className="weatherCondition">Sunny</div>
+            <div className="place">Kathmandu, Nepal</div>
+          </div>
+        </div>
+        <div className="date">{new Date().toLocaleString()}</div>
+        {/* Our 4 column section */}
+        <div className="extra-temp">
+          <div className="temp-info-minmax">
+            <div className="two-sided-section">
+              <p>
+                <i className="wi wi-sunset"></i>
+              </p>
+              <p className="extra-info-leftside">
+                19.19 PM <br /> Sunset
+              </p>
+            </div>
+
+            <div className="two-sided-section">
+              <p>
+                <i className="wi wi-humidity"></i>
+              </p>
+              <p className="extra-info-leftside">
+                19.19 PM <br /> Humidity
+              </p>
+            </div>
+          </div>
+          <div className="weather-extra-info">
+            <div className="two-sided-section">
+              <p>
+                <i className="wi wi-rain"></i>
+              </p>
+              <p className="extra-info-leftside">
+                19.19 PM <br /> Pressure
+              </p>
+            </div>
+
+            <div className="two-sided-section">
+              <p>
+                <i className="wi wi-strong-wind"></i>
+              </p>
+              <p className="extra-info-leftside">
+                19.19 PM <br /> Speed
+              </p>
+            </div>
+          </div>
+        </div>
       </article>
     </>
   );
